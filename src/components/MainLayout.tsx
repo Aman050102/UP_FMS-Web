46a48aa895e2af96067a85cc4b19061fcdaf1bfe
A-aman001 <aman.aleekae@gmail.com>
2026-02-12 16:37:57 +0700

    add header

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,40 +1,30 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import HeaderUser from "./MainHeader";
-import HeaderStaff from "./MainHeader";
-import SidebarUser from "./SidebarUser";
-import SidebarStaff from "./SidebarStaff";
+import MainHeader from "./MainHeader";
+import Sidebar from "./Sidebar";
 
 interface MainLayoutProps {
-  role: "user" | "staff";
+  role: "user" | "staff" | "assistant";
 }
 
 export default function MainLayout({ role }: MainLayoutProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const displayName = localStorage.getItem("display_name") || "ผู้ใช้งาน";
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
-    <div className="flex flex-col h-screen overflow-hidden bg-bg-main font-kanit">
-      {/* เลือก Header ตาม Role */}
-      {role === "staff" ? (
-        <HeaderStaff onToggleMenu={toggleMenu} displayName={displayName} />
-      ) : (
-        <HeaderUser onToggleMenu={toggleMenu} displayName={displayName} />
-      )}
-
+    <div className="flex flex-col h-screen overflow-hidden bg-[#f8fafc] font-kanit">
+      <MainHeader
+        onToggleMenu={() => setIsMenuOpen(!isMenuOpen)}
+        displayName={displayName}
+        role={role}
+      />
       <div className="flex flex-1 overflow-hidden mt-header">
-        {/* เลือก Sidebar ตาม Role */}
-        {role === "staff" ? (
-          <SidebarStaff
-            open={isMenuOpen}
-            onClose={() => setIsMenuOpen(false)}
-          />
-        ) : (
-          <SidebarUser open={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
-        )}
-
-        <main className="flex-1 overflow-y-auto p-5 custom-scrollbar bg-[#f8fafc]">
+        <Sidebar
+          role={role}
+          open={isMenuOpen}
+          onClose={() => setIsMenuOpen(false)}
+        />
+        <main className="flex-1 overflow-y-auto p-5 custom-scrollbar">
           <Outlet />
         </main>
       </div>
